5355ae53d1099e082a3444368b81450dda98662a
Igor Ziskin <izingor@gmail.com>
2022-04-21 15:37:09 +0300

    added removing show option

--- src/app/components/user-preview/user-preview.component.ts
+++ src/app/components/user-preview/user-preview.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { User } from 'src/app/models/user-model';
 
 @Component({
   selector: 'app-user-preview',
   templateUrl: './user-preview.component.html',
-  styleUrls: ['./user-preview.component.scss']
+  styleUrls: ['./user-preview.component.scss'],
 })
 export class UserPreviewComponent implements OnInit {
+  @Input() user?: User;
+  @Output() removeShow = new EventEmitter<number>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onRemoveShow(showIdx: number): void {
+    this.removeShow.emit(showIdx);
+    // console.log('user id', userId, 'show' , showIdx);
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/components/who-watch/who-watch.component.ts
+++ src/app/components/who-watch/who-watch.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user-model';
 
 @Component({
   selector: 'app-who-watch',
   templateUrl: './who-watch.component.html',
-  styleUrls: ['./who-watch.component.scss']
+  styleUrls: ['./who-watch.component.scss'],
 })
 export class WhoWatchComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  users: User[] = [
+    {
+      id: 101,
+      name: 'Roni',
+      shows: ['Breaking Bad', 'Game of thrones', 'Witcher'],
+    },
+    {
+      id: 102,
+      name: 'Makorni',
+      shows: ['Too hot to handle', 'Game of thrones', 'Witcher'],
+    },
+    {
+      id: 103,
+      name: 'Pepperoni',
+      shows: ['Better Call Saul', 'Game of thrones', 'Witcher'],
+    },
+  ];
 
-  ngOnInit(): void {
+  removeShow(showIdx: number, userIdx: number): void {
+
+    if(this.users) {
+      this.users?.[userIdx]?.shows?.splice(showIdx, 1);
+    }
+
   }
-
+  ngOnInit(): void {}
 }
